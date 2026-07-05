13560606815b3b5549b5450f4adba23c305b0412
mazhanbaig <mazhanbaig44@gmail.com>
2026-07-05 18:20:27 +0500

    aws keys intrgation

--- lib/aws-config.ts
+++ lib/aws-config.ts
@@ -4,12 +4,12 @@
 
 export const awsConfig = {
   cognito: {
-    userPoolId: '', // e.g. us-east-2_uXboG5pAb
-    userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-    region: '', // e.g. us-east-2
+    userPoolId:  process.env.NEXT_PUBLIC_AWS_USER_POOL_ID, // e.g. us-east-2_uXboG5pAb
+    userPoolClientId: process.env.NEXT_PUBLIC_AWS_COGNITO_CLIENT_ID, // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+    region: process.env.NEXT_PUBLIC_AWS_REGION, // e.g. us-east-2
   },
   api: {
-    invokeUrl: '', // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod
+    invokeUrl: process.env.INVOKE_URL, // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod
   },
 } as const;
 
