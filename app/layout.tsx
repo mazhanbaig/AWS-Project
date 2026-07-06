8d5f9b9973aaa724c75dc1f2f0454307e9c9c44b
Muhammad Azhan Baig <mazhanbaig44@gmail.com>
2026-07-06 13:33:56 +0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <body className="flex flex-col min-h-screen">
         <Navbar />
         <main className="flex-1 pt-16">{children}</main>
-        {/* <Footer /> */}
+         <Footer /> 
       </body>
     </html>
   );
